e84db050c64e97f83c35a7155c42c8887596ed91
Daniel Lasso <dlassoc123@gmail.com>
2023-03-21 18:15:10 -0500

    #5 punto del taller

--- Exercise/ejercicio.py
+++ Exercise/ejercicio.py
@@ -16,11 +16,24 @@ e2 = Elemento("hola")
 
 class Conjunto:
     CONTADOR = 0
-    def __init__(self, lista: list[Elemento]):
-        self.lista: list[Elemento] = lista
-        self.nombre = nombre
-        self.CONTADOR +=1
+    def __init__(self, nombre: str):
+        self.lista: list[Elemento] = []
+        self.nombre: str = nombre
+        self.__class__.CONTADOR +=1
+        self.__id = 2222
 
+    @property
+    def get_id(self):
+        return self.__id
+
+
+    def contiene(self):
+        print(e1|)
+
+c1 = Conjunto("a")
+print(c1.CONTADOR)
+print(c1.get_id)
+print(c1.contiene())
 
 
 print(e1 == e2)
