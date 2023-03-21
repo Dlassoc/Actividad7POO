871cfbfdcd7be733688be12831a6b1c3a2698771
Daniel Lasso <dlassoc123@gmail.com>
2023-03-21 13:31:07 -0500

    Ejercicio 1 hecho

---
+++ Exercise/ejercicio.py
@@ -0,0 +1,16 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+
+class Elemento:
+    nombre: str
+
+
+def __eq__(self, other):
+    return e1 == e2
+
+
+e1 = Elemento("hola")
+e2 = Elemento("hola")
+print(e1 == e2)
