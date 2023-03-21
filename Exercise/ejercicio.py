6a83161b7c3d29b6499b27f9a6417f27a5a7509e
Daniel Lasso <dlassoc123@gmail.com>
2023-03-21 13:37:36 -0500

    #p2 pendiente

--- Exercise/ejercicio.py
+++ Exercise/ejercicio.py
@@ -13,4 +13,14 @@ def __eq__(self, other):
 
 e1 = Elemento("hola")
 e2 = Elemento("hola")
+
+class Conjunto:
+    CONTADOR = 0
+    def __init__(self, lista: list[Elemento]):
+        self.lista: list[Elemento] = lista
+        self.nombre = nombre
+        self.CONTADOR +=1
+
+
+
 print(e1 == e2)
