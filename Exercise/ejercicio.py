3c2beabdcb952ef870d36671d9f7c5e518a8aad4
Daniel Lasso <dlassoc123@gmail.com>
2023-03-22 17:52:05 -0500

    mmm, intersectar no se, anda pendiente

--- Exercise/ejercicio.py
+++ Exercise/ejercicio.py
@@ -1,4 +1,5 @@
 import dataclasses
+from typing import Optional
 
 
 @dataclasses.dataclass
@@ -8,9 +9,6 @@ class Elemento:
 
 def __eq__(self, other):
     return e1 == e2
-
-
-
 
 
 class Conjunto:
@@ -32,16 +30,34 @@ class Conjunto:
     def agregar_elemento(self, other_element: Elemento):
         if self.contiene(other_element) is False:
             self.lista.append(other_element)
+            print(self.lista)
         else:
             print("El elemento ya existe, no se puede agregar")
 
+
+    def unir(self, c2):
+        if self.contiene(c2) is False:
+            self.lista.append(c2.lista)
+            print(f"aaaaaaaaaaaaaaa{self.lista}")
+
+    def __iadd__(self, c2):
+        self.lista += c2.lista
+        return self
+
+    def intersectar(self, c1, c2):
+        pass
 e1 = Elemento("hola")
 e2 = Elemento("hola")
 c1 = Conjunto("a")
-c1.lista.append((e1.nombre))
-print(c1.lista)
-c1.agregar_elemento("hola")
+c2 = Conjunto("b")
 # print(c1.CONTADOR)
 # print(c1.get_id)
-#print(c1.contiene("hola"))
+print(c1.contiene("hola"))
+c1.agregar_elemento("hola")
+print(c1.contiene("hola"))
+c1.agregar_elemento("perro")
+c2.agregar_elemento("pato")
+#c1.unir(c2)
+print(c1.lista + c2.lista) #Tengo dudas pero parece que funciona
+
 
