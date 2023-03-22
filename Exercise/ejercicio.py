918bdf1602cf399d88fb050f84e52bcac59062f3
Daniel Lasso <dlassoc123@gmail.com>
2023-03-22 15:51:49 -0500

    VERIFICADOOOOOOO, EXISTE O NO EL ELEMENTO Y SE AGREGA O NO

--- Exercise/ejercicio.py
+++ Exercise/ejercicio.py
@@ -30,7 +30,7 @@ class Conjunto:
         return other_element in self.lista
 
     def agregar_elemento(self, other_element: Elemento):
-        if self.contiene() is False:
+        if self.contiene(other_element) is False:
             self.lista.append(other_element)
         else:
             print("El elemento ya existe, no se puede agregar")
@@ -40,6 +40,7 @@ e2 = Elemento("hola")
 c1 = Conjunto("a")
 c1.lista.append((e1.nombre))
 print(c1.lista)
+c1.agregar_elemento("hola")
 # print(c1.CONTADOR)
 # print(c1.get_id)
 #print(c1.contiene("hola"))
